c9916c902f1a2d99e18ac52c80989ae93d774b26
Alexander Nosov <trashgenerator@gmail.com>
2016-05-02 22:56:26 +0200

    fix `update` function

--- src/update.js
+++ src/update.js
@@ -1,24 +1,24 @@
 const _ = require('lodash');
 
-const update = (obj, path, value) => {
+const update = (obj, path, nextValue) => {
   const pathArr = _.isArray(path) ? path : path.split('.');
   let newObj;
   if (!pathArr.length) {
-    return value;
+    return nextValue;
   } else {
     if (!obj[pathArr[0]]) {
       newObj = _.clone(obj);
-      _.set(newObj, pathArr, value);
+      _.set(newObj, pathArr, nextValue);
       return newObj;
     } else if (_.isArray(obj)) {
       newObj = _.clone(obj);
-      newObj[_.head(pathArr)] = update(newObj[_.head(pathArr)], _.tail(pathArr), value);
+      newObj[_.head(pathArr)] = update(newObj[_.head(pathArr)], _.tail(pathArr), nextValue);
       return newObj;
     } else {
       newObj = {};
       _.map(obj, (value, key, currentObj) => {
         if (key === pathArr[0]) {
-        newObj[key] = update(value, _.tail(pathArr), value);
+        newObj[key] = update(value, _.tail(pathArr), nextValue);
       } else {
         newObj[key] = obj[key];
       }
