6fc10dfb3fd110012ec595067140d7e1c8045288
Alexander Nosov <trashgenerator@gmail.com>
2016-05-15 21:28:17 +0200

    initialStateReducer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import setStateByPath from './setStateByPath';
 import replaceStateByPath from './setStateByPath';
 import wrapReducerWithSetGlobalState from './wrapReducerWithSetGlobalState';
 import connectSlicedState from './connectSlicedState';
+import initialStateReducer from './initialStateReducer';
 
 export {
   update,
   wrapReducerWithSetGlobalState,
   setStateByPath,
   replaceStateByPath,
-  connectSlicedState
+  connectSlicedState,
+  initialStateReducer,
 }
---
+++ src/initialStateReducer.js
@@ -0,0 +1,3 @@
+export default function initialStateReducer(initialState = {}) {
+  return (state = initialState, action) => state
+}
