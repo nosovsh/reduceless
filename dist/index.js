9c0e9e16c77bb49be9702ea06af2e44952530e0b
Alexander Nosov <trashgenerator@gmail.com>
2016-05-25 19:49:16 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -3661,8 +3661,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	exports.default = connectSlicedState;
 
 	var _react = __webpack_require__(21);
@@ -3685,38 +3683,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
 	/**
 	 * Connect provided component to `path` part of the redux state
 	 * @param path
-	 * @returns {getStateType(path)}
+	 * @returns {Function}
 	 */
 	function connectSlicedState(path) {
 	  return function (WrappedComponent) {
-	    var Connect = function (_React$Component) {
-	      _inherits(Connect, _React$Component);
-
-	      function Connect() {
-	        _classCallCheck(this, Connect);
-
-	        return _possibleConstructorReturn(this, Object.getPrototypeOf(Connect).apply(this, arguments));
-	      }
-
-	      _createClass(Connect, [{
-	        key: 'render',
-	        value: function render() {
-	          return _react2.default.createElement(WrappedComponent, this.props);
-	        }
-	      }]);
-
-	      return Connect;
-	    }(_react2.default.Component);
-
 	    return (0, _reactRedux.connect)(function (state) {
 	      return state;
 	    }, function (dispatch) {
@@ -3734,7 +3707,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	          return dispatch((0, _replaceStateByPath2.default)(path, newState));
 	        }
 	      });
-	    })(Connect);
+	    })(WrappedComponent);
 	  };
 	}
 
