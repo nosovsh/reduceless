54ce6c4875a017d74d0232ebf45c793dfeae4617
Alexander Nosov <trashgenerator@gmail.com>
2016-07-31 21:02:34 +0200

    do not rerender componet if state slice didn't change

---
+++ src/utils/defaultMemoize.js
@@ -0,0 +1,29 @@
+/**
+ * Memoizing function.
+ *
+ * Copied from `reselect` library.
+ * https://github.com/reactjs/reselect/blob/master/src/index.js
+ *
+ */
+
+
+function defaultEqualityCheck(a, b) {
+  return a === b
+}
+
+export function defaultMemoize(func, equalityCheck = defaultEqualityCheck) {
+  let lastArgs = null
+  let lastResult = null
+  return (...args) => {
+    if (
+      lastArgs !== null &&
+      lastArgs.length === args.length &&
+      args.every((value, index) => equalityCheck(value, lastArgs[index]))
+    ) {
+      return lastResult
+    }
+    lastResult = func(...args)
+    lastArgs = args
+    return lastResult
+  }
+}
--- src/withState.js
+++ src/withState.js
@@ -4,9 +4,17 @@ import get from 'lodash/get';
 import isFunction from 'lodash/isFunction';
 import setStateByPath from './setStateByPath';
 import replaceStateByPath from './replaceStateByPath';
+import {defaultMemoize} from './utils/defaultMemoize';
+
+// action creators that can be memoized
+const setStateCreator = (realPath, dispatch) => newState => dispatch(setStateByPath(realPath, newState));
+const  replaceStateCreator = (realPath, dispatch) => newState => dispatch(replaceStateByPath(realPath, newState));
 
 /**
  * Connect provided component to `path` part of the redux state
+ *
+ * Generated props are cached. So your component will not be rerendered if state slice didn't change.
+ *
  * @param path
  * @param stateName
  * @param setStateName
@@ -14,6 +22,10 @@ import replaceStateByPath from './replaceStateByPath';
  * @returns {Function}
  */
 export default function withState(path, stateName = 'state', setStateName = 'setState', replaceStateName = 'replaceState') {
+  // memoizing action creators.
+  const setStateCreatorMemoized = defaultMemoize(setStateCreator);
+  const replaceStateCreatorMemoized = defaultMemoize(replaceStateCreator);
+
   return (state, {dispatch}, props) => {
     const realPath = isFunction(path) ? path(props) : path;
     const slicedState = get(state, realPath);
@@ -24,10 +36,10 @@ export default function withState(path, stateName = 'state', setStateName = 'set
       result[stateName] = slicedState;
     }
     if (setStateName) {
-      result[setStateName] = newState => dispatch(setStateByPath(realPath, newState));
+      result[setStateName] = setStateCreatorMemoized(realPath, dispatch);
     }
     if (replaceStateName) {
-      result[replaceStateName] = newState => dispatch(replaceStateByPath(realPath, newState));
+      result[replaceStateName] = replaceStateCreatorMemoized(realPath, dispatch);
     }
     return result
   }
